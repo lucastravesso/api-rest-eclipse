64e9abd78bf159c0641d0044df8dc05c768d7cfb
Lucas Escobar <lucasescobar.ti@hotmail.com>
2021-03-12 17:50:39 -0300

    notação da classe crypt

--- main/src/main/java/com/projeto/main/crypt/Crypt.java
+++ main/src/main/java/com/projeto/main/crypt/Crypt.java
@@ -12,18 +12,24 @@ public class Crypt {
 	
 	public static String crypt(String senha)
 	{
+		//cria variavel retorno que armazena a senha criptografada
 		String retorno = "";
 		
+		//import do messageDigest que traz funcionalidade de hash
 		MessageDigest md;
+		//tenta o codigo
 		try {
-			
+			//define o hash(digest) como MD-5 de criptografia
 			md = MessageDigest.getInstance("MD5");
+			//cria um hash de bigInteger que vai pegar a senha em bytes
 			BigInteger hash = new BigInteger(1, md.digest(senha.getBytes()));
+			//adiciona em hash string a senha e diz que deve ser em hexadecimal o hash
 			retorno = hash.toString(16);
-			
+			//nao estou tratando erro.....
 		}catch(Exception e) {}
 		
 		return retorno;
+		//criado por Lusgas
 	}
 	
 }
